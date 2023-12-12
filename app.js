f843f652d02a06da67dcce493e5769ed77cfb309
1234567890970 <deepeshm064@gmail.com>
2023-12-12 13:04:35 +0530

    first commit

--- app.js
+++ app.js
@@ -3,16 +3,27 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const expressSession = require('express-session')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+const { session } = require('passport');
+const passport = require('passport');
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: false,
+  secret: "hello hey bye bye"  
+}));
+app.use(passport.initialize());
+app.use(passport.session()) // calls the deserializeUser function to req.user
+passport.serializeUser(usersRouter.serializeUser());
+passport.deserializeUser(usersRouter.deserializeUser());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
